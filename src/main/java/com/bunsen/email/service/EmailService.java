48276e0717269c9a920649cf8ad3e82c1a3ec194
Ishimwe-William <ishimwe.william2000@gmail.com>
2023-12-03 16:06:47 +0200

    Initial commit

---
+++ src/main/java/com/bunsen/email/service/EmailService.java
@@ -0,0 +1,62 @@
+package com.bunsen.email.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.util.Locale;
+
+@Service
+public class EmailService {
+    private static TemplateEngine templateEngine;
+    private static Context context;
+
+    private JavaMailSender emailSender;
+    private final Logger LOG= LoggerFactory.getLogger(EmailService.class);
+
+    @Autowired
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void prepareAndSendEmail() throws MessagingException {
+        String htmlTemplate="templates/emailTemplate.html";
+        String mailTo="bunsenplus.org@gmail.com";
+        initializeTemplateEngine();
+
+        context.setVariable("sender","Thymeleaf Email");
+        context.setVariable("mailTo",mailTo);
+
+        String htmlBody=templateEngine.process(htmlTemplate,context);
+        sendEmail(mailTo,"Bunsen Thymeleaf email demo",htmlBody);
+    }
+
+    private void sendEmail(String mailTo,String subject,String mailBody) throws MessagingException {
+        MimeMessage message=emailSender.createMimeMessage();
+        MimeMessageHelper helper=new MimeMessageHelper(message,true);
+        helper.setTo(mailTo);
+        helper.setSubject(subject);
+        helper.setText(mailBody,true);
+        emailSender.send(message);
+        LOG.info("Email sent to "+mailTo);
+    }
+
+    private static void initializeTemplateEngine(){
+        ClassLoaderTemplateResolver resolver=new ClassLoaderTemplateResolver();
+        resolver.setTemplateMode("HTML5");
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding("UTF-8");
+
+        templateEngine=new TemplateEngine();
+        templateEngine.setTemplateResolver(resolver);
+        context=new Context(Locale.ENGLISH);
+    }
+}
